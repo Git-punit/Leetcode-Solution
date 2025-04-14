ee2c52253a78ec4fd5fcea42ba006f9f7bf9c58e
PUNIT YADAV <137027839+Git-punit@users.noreply.github.com>
2025-04-14 10:41:38 +0530

    Update 22. Generate Parentheses.java

--- 22. Generate Parentheses.java
+++ 22. Generate Parentheses.java
@@ -1,15 +1,23 @@
-def generateParenthesis(self, n: int) -> List[str]:
-	def dfs(left, right, s):
-		if len(s) == n * 2:
-			res.append(s)
-			return 
-
-		if left < n:
-			dfs(left + 1, right, s + '(')
-
-		if right < left:
-			dfs(left, right + 1, s + ')')
-
-	res = []
-	dfs(0, 0, '')
-	return res
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        recurse(res, 0, 0, "", n);
+        return res;
+    }
+    
+    public void recurse(List<String> res, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
+            return;
+        }
+        
+        if (left < n) {
+            recurse(res, left + 1, right, s + "(", n);
+        }
+        
+        if (right < left) {
+            recurse(res, left, right + 1, s + ")", n);
+        }
+    }
+	// See above tree diagram with parameters (left, right, s) for better understanding
+}
