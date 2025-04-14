c91e5c98fa486755e372ef64db85d399a25da0a5
PUNIT YADAV <137027839+Git-punit@users.noreply.github.com>
2025-04-14 09:53:31 +0530

    Create 784. Letter Case Permutation.java

---
+++ 784. Letter Case Permutation.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    void recurse(char[] str, int pos, List<String> result) {
+        if (pos == str.length) {
+            result.add(new String(str));
+            return;
+        }
+        
+        if (Character.isLetter(str[pos])) {
+            if (Character.isUpperCase(str[pos])) {
+                str[pos] = Character.toLowerCase(str[pos]);
+                
+                recurse(str, pos + 1, result);
+                
+                str[pos] = Character.toUpperCase(str[pos]);
+            }
+            else {
+                str[pos] = Character.toUpperCase(str[pos]);
+                recurse(str, pos + 1, result);
+                str[pos] = Character.toLowerCase(str[pos]);
+            }
+        }
+        
+        recurse(str, pos + 1,  result);
+    }
+    
+    public List<String> letterCasePermutation(String S) {
+        List<String> result = new ArrayList<>();
+        recurse(S.toCharArray(), 0, result);
+        return result;
+    }
+}
