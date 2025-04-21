a3d7b2dd7f7eaab862b6825d2697c3577c07a67f
PUNIT YADAV <137027839+Git-punit@users.noreply.github.com>
2025-04-21 10:36:32 +0530

    Create 509. Fibonacci Number.java

---
+++ 509. Fibonacci Number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return fibhelper(n,dp);
+    }
+    public int fibhelper(int n, int[] dp) {
+        if(n == 0 || n ==1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int a = fibhelper(n-1,dp);
+        int b = fibhelper(n-2,dp);
+
+        dp[n]=a+b;
+        return a+b;
+    }
+}
